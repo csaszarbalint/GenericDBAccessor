5d09eb6af1acbc71f418844e0e1040b33aea4b8d
csaszarbalint <csaszar.balint@hbsz.edu.hu>
2025-09-17 09:44:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using MyDBAccessor;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace GenericDBAccessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            DbConnection connection = new MySqlConnection();
+
+            DBAccessor dbAccessor = new DBAccessor();
+        }
+    }
+}
